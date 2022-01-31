78b25acda1b172f322518a2cabbc75425692dc6f
furkan3458 <furkanuguz3458@gmail.com>
2022-01-31 19:14:09 +0300

    rememberme service fixes

--- src/main/java/com/example/service/CustomRememberMeService.java
+++ src/main/java/com/example/service/CustomRememberMeService.java
@@ -4,29 +4,28 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CustomRememberMeService implements RememberMeServices {
+import com.example.security.UserDetailsServiceImpl;
 
-	@Override
-	public Authentication autoLogin(HttpServletRequest request, HttpServletResponse response) {
-		return null;
+@Service
+public class CustomRememberMeService extends TokenBasedRememberMeServices {
+
+	private static UserDetailsServiceImpl userDetailsService = new UserDetailsServiceImpl();
+	
+	public CustomRememberMeService() {
+		super("rememberMe-key", userDetailsService);
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void loginFail(HttpServletRequest request, HttpServletResponse response) {
+	public void onLoginFail(HttpServletRequest request, HttpServletResponse response) {
 		Cookie cookie = new Cookie("last-username",request.getParameter("username"));
 		cookie.setMaxAge(86400);
 		cookie.setPath("/");
 		response.addCookie(cookie);
 	}
-
-	@Override
-	public void loginSuccess(HttpServletRequest request, HttpServletResponse response,
-			Authentication successfulAuthentication) {
-	}
-
 }
